800721d71db3ae43ff2dcb4af04cdd587036a369
unknown <peymantetra2357@gmail.com>
2022-04-04 12:13:39 -0700

    archive product  change

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -7,18 +7,10 @@ get_header( 'shop' );
 //do_action( 'woocommerce_before_main_content' );
 
 ?>
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
-
-	<?php
-//	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
+<!-- custom html top page here -->
 <?php
 if ( woocommerce_product_loop() ) {
-	do_action( 'woocommerce_before_shop_loop' );
+	// do_action( 'woocommerce_before_shop_loop' );
 
 	woocommerce_product_loop_start();
 
@@ -31,7 +23,7 @@ if ( woocommerce_product_loop() ) {
 	}
 	woocommerce_product_loop_end();
 
-	do_action( 'woocommerce_after_shop_loop' );
+	// do_action( 'woocommerce_after_shop_loop' );
 } else {
 	/**
 	 * Hook: woocommerce_no_products_found.
