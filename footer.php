20fff637b5d8f37cd0028e4754ca1cd6293d9928
alireza <areza9494@gmail.com>
2022-05-04 14:40:18 +0430

    create footer frontend

--- footer.php
+++ footer.php
@@ -1,5 +1,89 @@
 <footer>
-    <?php 
+    
+    <div class="footer-wrapper footer-tags">
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word 2">word 2</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word 4">word 4</a>
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word 2">word 2</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word 4">word 4</a>
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word 2">word 2</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word 4">word 4</a>
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word s da a">word s da a</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word as dasd sa">word as dasd sa</a>
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word 2">word 2</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word 4">word 4</a>
+        <a href="#" title="word 1">word 1</a>
+        <a href="#" title="word 2">word 2</a>
+        <a href="#" title="word 3">word 3</a>
+        <a href="#" title="word 4">word 4</a>
+    </div>
+
+    <div class="footer-wrapper footer-links">
+        <div class="footer-link-col">
+            <h4>title</h4>
+            
+                <a href="#" title="link">link</a>
+                <a href="#" title="link">link</a>
+                <a href="#" title="link">link</a>
+                <a href="#" title="link">link</a>
+                <a href="#" title="link">link</a>
+            
+        </div>
+        <div class="footer-link-col">
+            <h4>title</h4>
+            
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link lin kl in dkashd asuhd kasudhk kli nkli nkl ink lin nkli nklin k lin  kl ink">link lin kl in dkashd asuhd kasudhk kli nkli nkl ink lin nkli nklin k lin  kl ink</a>
+            
+        </div>
+        <div class="footer-link-col">
+            <h4>title</h4>
+            
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            
+        </div>
+        <div class="footer-link-col">
+            <h4>title</h4>
+            
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            <a href="#" title="link">link</a>
+            
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="footer-bottom-logo"></div>
+        <div class="footer-bottom-logo-text">
+        DoonDookStudio
+        </div>
+        <div class="footer-bottom-copyright">
+        Copyright © 2018-<?php echo date("Y"); ?> by DoonDookStudio All Rights Reserved.
+        </div>
+    </div>
+
+    <?php /*
         $terms = get_terms( array( 
             'taxonomy' => 'pa_genre',
             'fields' => 'all',
@@ -18,7 +102,7 @@
         echo '</ul>';
         // var_dump($terms);
         //////////////////////////////////////////////
-        echo "</br>";   
+        echo "</br>";  */ 
     ?>   
         
 
